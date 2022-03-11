84fbb8312c4096217ced019b6cfc3eef943004e2
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-10 16:32:05 -0800

    build: add struct complex to header file

--- holberton.h
+++ holberton.h
@@ -1,6 +1,20 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**
+ * struct complex - complex number
+ * @re: real number
+ * @im: imaginary number
+ *
+ * Description: Longer description
+ */
+
+typedef struct complex
+{
+	double re;
+	double im;
+} complex;
+
 void display_complex_number(complex c);
 
 #endif
