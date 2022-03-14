e85e3ff35488ec7e2a26b69db2806bbe057ffc08
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-13 23:02:11 -0700

    refactor

--- 0x00-math_complex/0-main.c
+++ 0x00-math_complex/0-main.c
@@ -10,15 +10,15 @@
 int main(void)
 {
     complex c1;
-    complex *p = &c1;
 
     c1.re = 1;
     c1.im = 2;
-    c1.ch = 'c';
-    c1.ptr = "hello";
-    p->ptr = "world";
-    (*p).ptr = "everyone";
-//    c1->ptr = "world";
     display_complex_number(c1);
+    c1.re = 1;
+    c1.im = 0;
+    display_complex_number(c1);
+    return (0);
+
+
     return (0);
 }
