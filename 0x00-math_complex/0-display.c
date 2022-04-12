e0d9b4cf605b9d982f6925cf899c39482839b103
Chiara Caprasi <91517809+chiaracaprasi@users.noreply.github.com>
2022-04-12 17:19:00 +1000

    fix: update 0-display.c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *void print_re - print real part of complet number
+ * @re: real number
+ *
+ * Return: 0 for success
+ */
+
+int print_re(double re)
+{
+	printf("%.0f", re);
+	return (0);
+}
+
+/**
+ *void print_im - print imaginary part of complet number
+ * @re: imaginary number
+ *
+ * Return: 0 for success
+ */
+
+int print_im(double im)
+{
+	if (im == 1)
+	{
+		printf("i");
+	}
+	if (im < 0)
+	{
+		im = im * -1;
+		printf("-");
+	}
+	if (im > 0)
+	{
+		printf("+ ");
+	}
+	
+	else
+	{
+		printf("%.0fi", im);
+	}
+}
 
 /**
  *void display_complex_number - displays the complex numbers
@@ -10,12 +52,17 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.im != 0)
+	if (c.im == 0 && c.re == 0)
 	{
-		printf("%.0f + %.0fi\n", c.re, c.im);
+		printf("0\n");
+		return;
 	}
 	else
 	{
-		printf("%.0f\n", c.re);
+		print_re(c.re);
+		printf(" "); 
+		print_im(c.im);
+		printf("\n");
 	}
+			
 }
